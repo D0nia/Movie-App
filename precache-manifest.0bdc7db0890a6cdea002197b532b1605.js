31ede837de07ab2aa71e66623d79559141dd3528
D0nia <salah3donia@gmail.com>
2019-09-05 15:38:41 +0100

    Updates

---
+++ precache-manifest.0bdc7db0890a6cdea002197b532b1605.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3d8bc4fb77b958e28f450eabfd1f4e3",
+    "url": "/Movie-App/index.html"
+  },
+  {
+    "revision": "191020b0caee2c47b583",
+    "url": "/Movie-App/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "9eb3874611e8462b5128",
+    "url": "/Movie-App/static/css/main.1b519d2b.chunk.css"
+  },
+  {
+    "revision": "191020b0caee2c47b583",
+    "url": "/Movie-App/static/js/2.df28ba7e.chunk.js"
+  },
+  {
+    "revision": "9eb3874611e8462b5128",
+    "url": "/Movie-App/static/js/main.c531a013.chunk.js"
+  },
+  {
+    "revision": "42fbc3f37b927f3d3ac0",
+    "url": "/Movie-App/static/js/runtime~main.df9e887b.js"
+  },
+  {
+    "revision": "2af3b489f4a8dfae804fcc68ade07255",
+    "url": "/Movie-App/static/media/beastAndBoy.2af3b489.jpg"
+  },
+  {
+    "revision": "ad8f9e855793bb414f79b180e67c9910",
+    "url": "/Movie-App/static/media/colorful.ad8f9e85.jpg"
+  },
+  {
+    "revision": "c9f6794969f53ad9318aded8e36ad9b9",
+    "url": "/Movie-App/static/media/kimiNoNawa.c9f67949.jpg"
+  },
+  {
+    "revision": "d53f8739aa184c688bef1af6084f6739",
+    "url": "/Movie-App/static/media/spiritedAway.d53f8739.jpg"
+  }
+]);
